b000e3203350f8a9b82b46ac5649de1ec3bf825d
MustafaGhouri <mustafaghouri22@gmail.com>
2023-10-02 07:01:12 +0500

    role

--- app/Http/Controllers/AppControllers/Auth/AuthController.php
+++ app/Http/Controllers/AppControllers/Auth/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
             if (!empty($user) && $user->status == 0) {
                 return response()->json(["res" => "error", "message" => "login failed because your account is not verified! Please check Your mail inbox!"]);
             }
-            if ($user->role_id == 2 || $user->role_id == 4) {
+            if ($user->role_id == 2 || $user->role_id == 3) {
                 $token = Auth::attempt(['email' =>  $request->email, 'password' => $request->password, "status" => "1"]);
 
                 if ($token) {
